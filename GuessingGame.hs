4fefb5e5cb57387797de5cfc828408180b4b8bf4
Eric Fulmer <EFulmer@gmail.com>
2016-02-18 13:02:14 -0500

    add debug flag and contrived lens example to GuessingGame

--- GuessingGame.hs
+++ GuessingGame.hs
@@ -1,4 +1,7 @@
 module GuessingGame where
+import Control.Lens 
+import Control.Monad (when)
+import System.Environment (getArgs)
 import System.Random (randomRIO)
 import Text.Read (readMaybe)
 
@@ -22,26 +25,31 @@ parseError = (False, "Please enter a number!")
 
 checkGuess :: String -> Int -> Result
 checkGuess guess answer = let guess' = (readMaybe guess :: Maybe Int) 
-                              inRange x (a, b) = x `elem` [a..b] in
-    case guess' of (Just guess'') -> if guess'' `inRange` guessRange
+                              inRange x (a, b) = x `elem` [a..b]
+                              inRng x rng = rng ^. _1 <= x && x <= rng ^._2 in
+    case guess' of (Just guess'') -> if guess'' `inRng` guessRange
                                         then if guess'' == answer 
                                              then win
                                              else tryAgain
                                         else badRange
                    Nothing        -> parseError
 
-guessingGame :: Int -> IO ()
-guessingGame answer = do
-    putStrLn $ "Guess a number between " ++ (show . fst) guessRange ++ " and " ++ (show . snd) guessRange ++ "."
+guessingGame :: Int -> Bool -> IO ()
+guessingGame answer debug = do
+    putStrLn $ "Guess a number between " ++ 
+        (show . fst) guessRange ++ " and " ++ (show . snd) guessRange ++ "."
+    when debug (putStrLn $ "(Answer = " ++ show answer ++ ")")
     putStr "Make your guess! "
     guess <- getLine
     putStrLn $ "You guessed: " ++ guess
     let (won, response) = checkGuess guess answer
     putStrLn response
-    if won then return () else guessingGame answer
+    if won then return () else guessingGame answer debug
     
 main :: IO ()
 main = do
+    args <- getArgs
     answer <- randomRIO guessRange
     putStrLn "Welcome to the guessing game!"
-    guessingGame answer
+    let debug = length args > 0 && (args !! 0 == "--debug")
+    guessingGame answer debug
